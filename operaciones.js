4327671db2480288d6403c77a8e042dffc3d4aaf
jeanpaul <juanpablomulcue13@gmail.com>
2024-03-13 21:09:32 -0500

    operciones1

---
+++ operaciones.js
@@ -0,0 +1,21 @@
+console.log
+let counter = 100;
+// suma 
+counter = counter +20; //
+counter += 30;//
+//resta
+counter = counter -20; //
+counter -=30;//
+//multiplicacion
+counter = counter * 3; //
+counter *= 4; //
+// division
+counter = counter / 5; //
+counter /= 4; //
+// residuo
+counter = counter % 3; //
+counter %=3; //
+//exponenciación
+counter = counter ** 3; //
+counter **= 2;//
+
---
+++ operaciones1.js
@@ -0,0 +1,5 @@
+let counter = "100";
+let age = 100;
+let result = counter == age;
+
+console.log(result)
